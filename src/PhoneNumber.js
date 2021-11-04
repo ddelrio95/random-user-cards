5815ee20add8ab5517d4361198e62a3a26569321
Dario del Rio <ddelrio95@yahoo.com>
2021-11-03 21:39:04 -0400

    final

---
+++ src/PhoneNumber.js
@@ -0,0 +1,10 @@
+import './PhoneNumber.css'
+const PhoneNumber = ({ number, type }) => (
+  <div className='phoneNumber'>
+    <p>
+      {type}: {number}
+    </p>
+  </div>
+)
+
+export default PhoneNumber
